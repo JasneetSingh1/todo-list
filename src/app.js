6d81c06f198538ccedda2958509a5066d0adaf6a
Jasneet Singh <100010929+JasneetSingh1@users.noreply.github.com>
2025-02-09 18:35:02 -0700

    create template html and styles

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import { Todo } from "./todo";
 import { Project } from "./project";
+import "./styles.css";
 
 const project = new Project("Daily Tasks")
 const toDo = new Todo("Laundry", "Laundry Time!", "2025-02-09", "Low")
